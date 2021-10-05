e284a4f73b470cc6462da511a7d1a7b7e73f16aa
HarshKumar04 <kumarharsh1367@gmail.com>
2021-10-05 14:06:56 +0530

    exercise 5 done

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var inputBox = document.querySelector("textarea");
+var outputBox = document.querySelector("#outputBox");
 // var anchorTag = document.querySelector("a[href='home']");
-// console.log(anchorTag);
+// console.log(outputBox);
 
 // inputBox.innerText = "Hii i am harsh";
 
+
 btnTranslate.addEventListener("click",function clickHandler(){
-    var output = inputBox.value;
-    console.log(output);
+    var userInput = inputBox.value;
+    outputBox.innerText = "ni amboj ala: translation of "+ userInput;
 });
 
